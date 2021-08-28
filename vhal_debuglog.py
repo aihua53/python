2a2ef375c8f4744683bb54d1b77682fa7d0eeb2f
aihua53 <aihua53@163.com>
2021-08-29 00:07:27 +0800

    update vhal debuglog

--- vhal_debuglog.py
+++ vhal_debuglog.py
@@ -2,13 +2,27 @@ import re, os
 # import pandas as pd
 # from openpyxl import load_workbook
 
+
 def parse_vhal(file):
     with open(file,mode="r",encoding='utf-8') as f:
-        content = f.readlines()
-        for line in content:
-            re_result = re.search('current SPI can data size:(\d)+summation SPI Can data size(\d)+SPICanInvokedCount=(\d)',line)
+        while True:
+            try:
+                line = f.readline()
+            except UnicodeDecodeError:
+                # print("'utf-8' codec can't decode byte:",line)
+                continue
+            # if not line:
+            if len(line) == 0:
+                print("done")
+                break
+            re_result = re.search('current SPI can data size:(\d+),\s+summation SPI Can data size:(\d+),\s+SPICanInvokedCount=(\d+),\s+timediff=(\d+)',line)
             if re_result != None:
-                print(re_result)
+                (spi_data_size,spi_accumulate,spi_count,spi_time)=re_result.groups()
+                data = line.split()[0]
+                time = line.split()[1]
+                print(data,time,spi_data_size,spi_accumulate,spi_count,spi_time)
+
+
 
 def main():
     parse_vhal(r'D:\github\python\test.log')

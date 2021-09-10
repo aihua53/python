2702e85668f91d29f85931250122974d259996e4
aihua53 <aihua53@163.com>
2021-09-10 18:40:00 +0800

    update vhal_pash shell

--- vhal/vhal_parse.sh
+++ vhal/vhal_parse.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
-#cpu
+
+#system cpu
+grep "\"cpu\"" vhal_analysis.log|awk '{print $2,$8}'|awk -F{ '{print $1,$NF}'|awk -F, '{print $1,$(NF-3)}'|awk -F\" '{print $1,$NF}'|awk -F. '{print $1,$(NF-1)}'|awk '{print $1,$3}'|awk -F' :' '{print $1,$2}'>system_cpu.txt
+
+#vhal cpu
 grep "vendor.ts.hardware.automotive.vehicle@2.0-service level:" vhal_analysis.log |awk '{print $2,$NF}'|sed 's/level://g'|awk -F. '{print $1,$2}'|awk '{print $3,$1}'|uniq -1|awk '{print $2,$1}'>vhal_cpu.txt
 
+#datacollector cpu
 grep "chj_datacollector level:" vhal_analysis.log |awk '{print $2,$NF}'|sed 's/level://g'|awk -F. '{print $1,$2}'|awk '{print $3,$1}'|uniq -1|awk '{print $2,$1}'>datacollector_cpu.txt
 
 #spi count

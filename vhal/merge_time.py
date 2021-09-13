4b75ffdc35dd5da729dd84b175738833903ae547
aihua53 <aihua53@163.com>
2021-09-13 11:34:38 +0800

    update merge_time

--- vhal/merge_time.py
+++ vhal/merge_time.py
@@ -3,69 +3,63 @@ import pandas as pd
 from openpyxl import load_workbook
 
 file_path = r'D:\github\python\vhal'#待分析目录
-vhal_cpu_file = os.path.join(file_path, f'vhal_cpu.txt')
-spi_size_file = os.path.join(file_path, f'spi_size.txt')
-spi_count_file = os.path.join(file_path, f'spi_count.txt')
-eth_size_file = os.path.join(file_path, f'eth_size.txt')
-eth_count_file = os.path.join(file_path, f'eth_count.txt')
-datacollector_cpu_file = os.path.join(file_path, f'datacollector_cpu.txt')
+file_name = []
+list_obj = []
 
-#parsed file
-valuefile = spi_count_file 
-cpu_file = vhal_cpu_file
-valuefile_2 = datacollector_cpu_file 
+# to be parsed file
+file_name.append("system_cpu")
+file_name.append("vhal_cpu")
+file_name.append("spi_count")
+file_name.append("spi_size")
+file_name.append("eth_count")
+file_name.append("eth_size")
+
+#parse time
+start_time = '17:29:05'
+end_time = '18:49:54'
+
+class To_be_analysed:
+    def __init__(self,name,file):
+        self.name = name
+        self.file = file
+        self.dict = {}
+
+    def create_dict(self):
+        with open(self.file,mode='r',encoding='utf-8') as f:
+            content = f.readlines()
+            self.dict.clear()
+            for line in content:
+                self.dict.update({line.split()[0]:line.split()[1]})
+
 
 def main():
-    with open(cpu_file,mode='r',encoding='utf-8') as f:
-        content_cpu = f.readlines()
-    cpu_dict = {}
+    for fn in file_name: 
+        obj = To_be_analysed(fn,os.path.join(file_path,fn+".txt"))
+        obj.create_dict()
+        list_obj.append(obj)
 
-    with open(valuefile,mode='r',encoding='utf-8') as f:
-        content_value = f.readlines()
-    value_dict = {}
-
-    with open(valuefile_2,mode='r',encoding='utf-8') as f:
-        content_value2 = f.readlines()
-    value2_dict = {}
-    
-    for line in content_cpu:
-        cpu_dict.update({line.split()[0]:line.split()[1]})
- 
-    for line in content_value:
-        value_dict.update({line.split()[0]:line.split()[1]})
-
-    for line in content_value2:
-        value2_dict.update({line.split()[0]:line.split()[1]})
-
-    start_time = '21:20:51'
-    end_time = '22:28:00'
     time1 = datetime.datetime.strptime(start_time,'%H:%M:%S')
     cur_time = start_time
     parse_result = []
 
     while(cur_time != end_time):
         dict_result = {}
-        if({cur_time}.issubset(cpu_dict.keys())):
-            dict_result['time'] = cur_time
-            dict_result['cpu'] = cpu_dict[cur_time]
-
-        if({cur_time}.issubset(value_dict.keys())):
-            dict_result['time'] = cur_time
-            dict_result['value'] = value_dict[cur_time]
-
-        if({cur_time}.issubset(value2_dict.keys())):
-            dict_result['time'] = cur_time
-            dict_result['value2'] = value2_dict[cur_time]
-        
-        parse_result.append(dict_result)
+        for obj in list_obj:
+            if({cur_time}.issubset(obj.dict.keys())):
+                dict_result['time'] = cur_time
+                dict_result[obj.name] = obj.dict[cur_time]
+        if (dict_result != {}):
+            parse_result.append(dict_result)
         time1 = time1 + datetime.timedelta(seconds=1)
         cur_time = time1.strftime("%H:%M:%S")
-    
+
     result_path = os.path.join(file_path, f'parse_result.csv')
     if os.path.exists(result_path) == True:
         os.remove(result_path)
 
     pf = pd.DataFrame(parse_result)
     pf.to_csv(result_path, mode='w', index=False)
-    print("done")
+    # pf.to_excel(result_path,sheet_name="test",index=False)
+    print("well done")
+
 main()
